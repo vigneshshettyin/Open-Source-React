5003042ed0ac5a876d15b582090944e32e380e7d
Sukriti Sood <55010599+Sukriti-sood@users.noreply.github.com>
2021-05-23 19:49:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import Home from "./pages/Home";
-
-import Covid from "./pages/About_Us";
-import Climate from "./pages/Climate";
-import Image from "./pages/Image";
-import Vaccine from "./pages/Covid-19-Vac";
-import Ip_Info from "./pages/Ip_Info";
-import Discord from "./pages/Discord";
+import Covid from "./pages/Covid-19 Tracker App/Covid-19_Tracker";
+import Climate from "./pages/Climate App/Climate";
+import Image from "./pages/Image Live Search/Image";
+import Vaccine from "./pages/Covid-19 Vaccine/Covid-19-Vac";
+import Ip_Info from "./pages/IP App/Ip_Info";
+import Discord from "./pages/Discord Webhook/Discord";
 import Quotes from "./pages/Quotes"
 
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
