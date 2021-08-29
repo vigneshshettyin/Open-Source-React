296b9988d663df4e834dd2217b166ad69eab7065
Vignesh Shetty <77713888+vigneshshettyin@users.noreply.github.com>
2021-08-29 16:18:42 +0530

    Fixed Quote!

--- src/pages/Quotes/Quotes.js
+++ src/pages/Quotes/Quotes.js
@@ -17,35 +17,37 @@ function Quotestr(props) {
 const Quote = ({ selectedquote, selectrandindex }) => {
   return (
     <>
-      <Quotestr
-        text={selectedquote() && selectedquote().quote}
-        author={selectedquote() && selectedquote().author}
-      />
-      <button
-        onClick={() => {
-          window.location.href = `https://twitter.com/intent/tweet?url=&text=${
-            selectedquote() && selectedquote().quote
-          }`;
-        }}
-        className="button tweet"
-        title="Tweet this quote"
-      >
-        <AiIcons.AiOutlineTwitter />
-      </button>
-      <button
-        onClick={() => {
-          window.location.href = `http://www.tumblr.com/share?v=3&u=&t=${
-            selectedquote() && selectedquote().quote
-          }`;
-        }}
-        className="button tumbler"
-        title="Share on Tumbler"
-      >
-        t
-      </button>
-      <button className="button newquote" onClick={selectrandindex}>
-        New Quote
-      </button>
+      <div className="getBackColour">
+        <Quotestr
+          text={selectedquote() && selectedquote().quote}
+          author={selectedquote() && selectedquote().author}
+        />
+        <button
+          onClick={() => {
+            window.location.href = `https://twitter.com/intent/tweet?url=&text=${
+              selectedquote() && selectedquote().quote
+            }`;
+          }}
+          className="button tweet"
+          title="Tweet this quote"
+        >
+          <AiIcons.AiOutlineTwitter />
+        </button>
+        <button
+          onClick={() => {
+            window.location.href = `http://www.tumblr.com/share?v=3&u=&t=${
+              selectedquote() && selectedquote().quote
+            }`;
+          }}
+          className="button tumbler"
+          title="Share on Tumbler"
+        >
+          t
+        </button>
+        <button className="button newquote" onClick={selectrandindex}>
+          New Quote
+        </button>
+      </div>
     </>
   );
 };
@@ -53,20 +55,6 @@ const Quote = ({ selectedquote, selectrandindex }) => {
 export default function App() {
   const [datarray, setdataarray] = useState([]);
   const [srandind, setranind] = useState(0);
-  var colors = [
-    "#16a085",
-    "#27ae60",
-    "#2c3e50",
-    "#f39c12",
-    "#e74c3c",
-    "#9b59b6",
-    "#FB6964",
-    "#342224",
-    "#472E32",
-    "#BDBB99",
-    "#77B1A9",
-    "#73A857",
-  ];
   useEffect(() => {
     function getfetchurl() {
       return "https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json";
@@ -75,23 +63,11 @@ export default function App() {
     const fetchdata = async () => {
       const result = await Axios(getfetchurl());
       const quotes = result.data;
-      // console.log("quotes", quotes);
       setdataarray(quotes);
-      //   .then(()=>{
-      //   console.log(datarray.length);
-      //   });
       setranind(Math.floor(Math.random() * datarray.length));
     };
-    fetchdata().then(() => {
-      // console.log(datarray.length);
-    });
+    fetchdata().then(() => {});
   }, [datarray.length]);
-  useEffect(() => {
-    document.body.style.backgroundColor =
-      colors[Math.floor(Math.random() * colors.length)];
-    // document.getElementsByClassName("button").style.backgroundColor=colors[Math.floor(Math.random()*(colors.length))];
-    // console.log(document.getElementsByClassName("button"));
-  });
 
   function quoteselection() {
     if (!datarray.length || !Number.isInteger(srandind)) {
